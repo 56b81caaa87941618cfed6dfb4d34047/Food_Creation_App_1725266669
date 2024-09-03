2942fdf68e7c8d78e91d9edb92ddc91de0171f52
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-03 07:22:27 +0000

    Reset file
    File: components/simple_cta_component_1725266672.js
    Git Ref: 24052cea2739e139d769520243320115cd2daf1b
    Prune: False

--- components/simple_cta_component_1725266672.js
+++ components/simple_cta_component_1725266672.js
@@ -20,11 +20,7 @@ INSTRUCTION: Summary: This component contains a h2 heading title for the CTA, a 
                 <div id="cta-button-container" class="flex">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                         STARTER_TEMPLATE_CALL_TO_ACTION_BUTTON_TEXT
-                        STARTER_TEMPLATE_CALL_TO_ACTION_BUTTON_TEXT
                     </a>
-                    <button id="free-button" class="flex-1 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
-                        Free
-                    </button>
                 </div>
             </div>
         </div>
